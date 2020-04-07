5ad2c74a5e5a629c09552de6e329559a68048f18
29244 <29244@ma-web.nl>
2020-04-07 15:33:57 +0200

    new ajax1

--- AJAX1/form_1.php
+++ AJAX1/form_1.php
@@ -3,12 +3,17 @@
     $lastName = $_GET['lastName'];
     $age = $_GET['age'];
     $email = $_GET['email'];
-    echo "<h2>Response Demo Form</h2><h3> ";
+    echo "<h3>Response Demo Form</h3><h4> ";
     echo "You submitted the following information<br><ul>";
-    echo "<li>First name: <strong> $firstName</strong></li>";
-    echo "<li>Last name: <strong> $lastName</strong></li>";
+    echo "<li>Name  Lastname: <strong> $firstName  $lastName</strong></li>";
     echo "<li>Age:  $age</li>";
-    echo "<li>E-mail:  $email</li>";
-    echo "</li></ul></h3>";
-    echo "</li></ul></h3>";
+    echo "<li>E-mail  $email</li>";
+    echo "</li></ul></h4>";
+    dateStamp();
+
+    function dateStamp(){
+        echo "From received at:<br>";
+        echo "Date " . date("d-m-Y") . "<br>";
+        echo "Time " . date("H:i:s");
+    }
 ?>
